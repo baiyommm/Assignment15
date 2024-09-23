6ac7d116f25453d6efc1b434989b5fa049988fb4
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-23 11:08:28 -0700

    Completed the program to print a triangle of stars with 5 stars in the last row.

--- Assignment15/main.cpp
+++ Assignment15/main.cpp
@@ -1,14 +1,18 @@
-//
-//  main.cpp
-//  Assignment15
-//
-//  Created by Chatchapong Thuaypha on 9/23/24.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    int rows = 5; // Number of rows for the triangle
+    
+    for (int i = 1; i <= rows; i++) {
+        // Print stars for the current row
+        for (int j = 1; j <= i; j++) {
+            // Nested for loop to print the stars (j)
+            cout << "*";
+        }
+        // Next line
+        cout << endl;
+    }
     return 0;
 }
